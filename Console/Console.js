e8308d8ba0427383a5b7fd486a2b9461f5ad59eb
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2022-07-03 19:31:50 +0200

    Fixes

--- Console/Console.js
+++ Console/Console.js
@@ -209,70 +209,84 @@ export default class Console {
   }
 
   #HFH(htmlEl) {
-    let str1Reg = /"((?:\\"|(?!").)*)"/g,
-      str2Reg = /'((?:\\'|(?!').)*)'/g,
-      str3Reg = /`((?:\\`|(?!`).)*)`/g,
-      specialReg =
-        /\b(new|var|let|const|if|do|function|class|while|switch|of|in|for|return|continue|break\b)(?!\w)/g,
+    let str1Reg = /((?<=(?<!\\)(?:\\{2})*))"((?:\\"|(?!").)*)"/g,
+      str2Reg = /((?<=(?<!\\)(?:\\{2})*))'((?:\\'|(?!').)*)'/g,
+      str3Reg = /((?<=(?<!\\)(?:\\{2})*))`((?:\\`|(?!`).)*)`/g,
+      keywordReg =
+        /\b(import|from|interface|new|typeof|var|let|const|if|else|do|function|class|while|switch|try|catch|of|in|for|return|continue|break|throw\b)(?!\w)/g,
       typeReg =
-        /\b(Array|String|Object|Number|null|undefined|true|false|\$\b)(?!\w)/g,
-      methodReg = /\b(?!function|if|catch|while|for\b)\w+\s*(?=\(.*\))/g,
+        /\b(window|globalThis|self|this|Array|String|Object|Number|null|undefined|true|false|\$\b)(?!\w)/g,
+      methodReg = /\b(?![A-Z]|function|if|catch|while|for\b)\w+\s*(?=\(.*\))/g,
       commentReg =
         /(?:\/\*(?:[\s\S]*?)\*\/)|(?:^\s*\/\/(?:.*)$)|(?:_\s*\w+(?!\w*\(.*\)*))/g,
       classReg = /(?<![a-z])(?=\.*\s*)[A-Z]+\w*/g,
-      propretyReg = /(?<=\.\s*)\w+(?!\w*\(.*\))/g,
-      regexReg = /(?:\/([\s\S]+?)\/([gimsuy]*))/g;
+      propretyReg = /(?<=\.\s*)\w+(?!\w*\(.*\))/g;
 
     let string = htmlEl.innerHTML;
     let parsed = string.replace(
       str1Reg,
-      `<span data-hl-type="string" style="color: #a1e46d; font-style: normal;">&quot;$1&quot;</span>`
-    ); 
+      `$1<span data-hl-type="string" style="color: ${this.options.style.strings};">&quot;$2&quot;</span>`
+    );
     parsed = parsed.replace(
       str2Reg,
-      `<span data-hl-type="string" style="color: #a1e46d; font-style: normal;">&apos;$1&apos;</span>`
+      `<span data-hl-type="string" style="color: ${this.options.style.strings};">$1&apos;$2&apos;</span>`
     );
     parsed = parsed.replace(
       str3Reg,
-      `<span data-hl-type="string" style="color: #a1e46d; font-style: normal;">&#96;$1&#96;</span>`
+      `<span data-hl-type="string" style="color: ${this.options.style.strings};">$1&#96;$2&#96;</span>`
     );
     parsed = parsed.replace(
-      specialReg,
-      `<span data-hl-type="special" style="color: #b61fff;">$&</span>`
+      keywordReg,
+      `<span data-hl-type="keyword" style="color: ${this.options.style.keywords};">$&</span>`
     );
     parsed = parsed.replace(
       typeReg,
-      `<span data-hl-type="type" style="color: #e05f5f; font-style: italic;">$&</span>`
+      `<span data-hl-type="type" style="color: ${this.options.style.types}; font-style: italic;">$&</span>`
     );
     parsed = parsed.replace(
       methodReg,
-      `<span data-hl-type="method" style="color: #4dbfd4;">$&</span>`
+      `<span data-hl-type="method" style="color: ${this.options.style.methods};">$&</span>`
     );
     parsed = parsed.replace(
       propretyReg,
-      `<span data-hl-type="proprety" style="color: #eb993a;">$&</span>`
+      `<span data-hl-type="proprety" style="color: ${this.options.style.propreties};">$&</span>`
     );
-    // parsed = parsed.replace(
-    //   regexReg,
-    //   `<span data-hl-type="regex" style="color: #eb993a;">&#47;$1&#47;$2</span>`
-    // );
     parsed = parsed.replace(
       classReg,
-      `<span data-hl-type="class" style="color: #dee059;">$&</span>`
+      `<span data-hl-type="class" style="color: ${this.options.style.classes};">$&</span>`
     );
+
     parsed = parsed.replace(
       commentReg,
-      `<span data-hl-type="comment" style="color: #aaa; font-style: italic;">$&</span>`
+      `<span data-hl-type="comment" style="color: ${this.options.style.comments}; font-style: italic;">$&</span>`
     );
 
-    htmlEl.innerHTML = parsed;
+    htmlEl2.innerHTML = parsed;
 
-    htmlEl.querySelectorAll("span[data-hl-type]>span").forEach((el) => {
+    htmlEl2.querySelectorAll("span[data-hl-type]>span").forEach((el) => {
       el.style.color = "inherit";
       el.style.fontStyle = "inherit";
       el.style.fontWheight = "inherit";
       delete el.dataset.hlType;
     });
+
+    htmlEl2
+      .querySelectorAll("span[data-hl-type='string']>span")
+      .forEach((el) => {
+        el.style.color = "inherit";
+        el.style.fontStyle = "inherit";
+        el.style.fontWheight = "inherit";
+        delete el.dataset.hlType;
+      });
+
+    htmlEl2
+      .querySelectorAll("span[data-hl-type='comment']>span")
+      .forEach((el) => {
+        el.style.color = "inherit";
+        el.style.fontStyle = "inherit";
+        el.style.fontWheight = "inherit";
+        delete el.dataset.hlType;
+      });
   }
 
   createTile(
